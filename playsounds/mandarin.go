f649cb2bea742ed17aab92632901b1c8fd071737
Ed <ed0176@cchntek.com>
2023-09-06 15:31:03 +0800

    fix the tens place

--- playsounds/mandarin.go
+++ playsounds/mandarin.go
@@ -10,14 +10,31 @@ func MandarinRules(callNumber, counterNumber int) []string {
 
 	n := strconv.Itoa(callNumber)
 	c := strconv.Itoa(counterNumber)
-	callNumberSlice := strings.Split(n, "")
-	counterNumberSlice := strings.Split(c, "")
+
+	var callNumberSlice, counterNumberSlice []string
 
 	all := []string{"來賓"}
+	if callNumber >= 10 && callNumber < 100 && callNumber%10 == 0 {
+		callNumberSlice = []string{strconv.Itoa(callNumber)}
+	} else {
 
-	if len(n) <= 2 {
-		LessHundred(&callNumberSlice)
-		LessHundred(&counterNumberSlice)
+		callNumberSlice = strings.Split(n, "")
+		if len(n) == 2 {
+
+			LessHundred(&callNumberSlice)
+		}
+	}
+	//如果為十位數, 並且個位數為0
+	if counterNumber >= 10 && counterNumber < 100 && counterNumber%10 == 0 {
+		counterNumberSlice = []string{strconv.Itoa(counterNumber)}
+
+	} else {
+
+		counterNumberSlice = strings.Split(c, "")
+		if len(c) == 2 {
+
+			LessHundred(&counterNumberSlice)
+		}
 	}
 
 	all = append(all, callNumberSlice...)
@@ -32,7 +49,9 @@ func MandarinRules(callNumber, counterNumber int) []string {
 func LessHundred(n *[]string) {
 
 	number := *n
-
+	if number[len(number)-1] == "0" {
+		return
+	}
 	for i, v := range number {
 		if i == 0 {
 			number[i] = fmt.Sprintf("%s%s", v, "0")
